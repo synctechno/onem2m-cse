1e513a299e55495e8050f9d9da4ed66e88c5b517
Sherzod Elamanov <elamanov@synctechno.com>
2023-03-09 18:18:08 +0900

    http fix content-type handling

--- src/bindings/http/routes.ts
+++ src/bindings/http/routes.ts
@@ -1,15 +1,25 @@
+import {FastifyInstance} from "fastify";
 import {headerSchema} from "./schemas.js";
 import {resourceTypeEnum} from "../../types/types.js";
 import {httpToPrimitive, responsePrimitiveToHtpp} from "./converter.js";
 import {responsePrimitive} from "../../types/primitives.js";
 import {cseCore} from "../../index.js";
 
-export const router = (fastify, options, done) => {
-    fastify.addHook('onRequest', (request, reply, done) => {
-        const type = request.headers['content-type']
-        if(type.startsWith('application/vnd.onem2m-res+json')){
-            request.headers['content-type'] =
-                request.headers['content-type'].replace('application/vnd.onem2m-res+json', 'application/json')
+export const router = (fastify: FastifyInstance, options, done) => {
+    fastify.addHook('onRequest', (req, res, done) => {
+        if (req.method === 'POST' || req.method === 'PUT'){
+            if (req.headers['content-type'] === undefined){
+                res.code(400);
+            }
+            const type = req.headers['content-type'];
+            if (type === undefined){
+                res.code(400);
+            } else {
+                if(type.startsWith('application/vnd.onem2m-res+json')){
+                    req.headers['content-type'] =
+                        req.headers['content-type']!.replace('application/vnd.onem2m-res+json', 'application/json')
+                }
+            }
         }
         done()
     })
@@ -22,15 +32,15 @@ export const router = (fastify, options, done) => {
     }, async (req, res) => {
         let ty: resourceTypeEnum | undefined = undefined;
         if (req.method === 'POST' || req.method === 'PUT'){
-            ty = Number(req.headers['content-type'].split(';ty=')[1]);
+            ty = Number(req.headers['content-type']!.split(';ty=')[1]);
         }
 
         const reqPrimitive = httpToPrimitive(req, ty);
 
         const resPrimitive: responsePrimitive = await cseCore.primitiveGateway(reqPrimitive);
         const {headers, body, statusCode} = responsePrimitiveToHtpp(resPrimitive);
-        res.headers = headers;
-        res.code(statusCode).send(body);
+        res.headers(headers);
+        res.code(statusCode || 500).send(body);
     })
     done();
 };
